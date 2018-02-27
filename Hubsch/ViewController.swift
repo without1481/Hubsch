0ad0b1fd664d01104838728271c8c26d3a173f76
Petr Yanenko <without1481@gmail.com>
2018-02-27 08:28:24 +0200

    Initial Commit

---
+++ Hubsch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hubsch
+//
+//  Created by Piter Stivenson on 27.02.2018.
+//  Copyright © 2018 iPiterGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
